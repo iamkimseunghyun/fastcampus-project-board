d1a9f3d1ea6bb583f80789d26e7ecdb26b21dc31
kaka <iamkimseunghyun@gmail.com>
2022-09-11 21:07:00 +0900

    #21 - 페이지네이션
    구현하지 않은 articleCommentService 코드 구현 및 Css 파일 경로 수정

--- src/main/java/io/laaf/fastcampusprojectboard/controller/ArticleController.java
+++ src/main/java/io/laaf/fastcampusprojectboard/controller/ArticleController.java
@@ -36,6 +36,7 @@ public class ArticleController {
 
         Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
 
