e0704260eabcfce59346cf6c3a0086b919ff9edf
kaka <iamkimseunghyun@gmail.com>
2022-09-20 09:44:04 +0900

    #36 - 게시글 뷰 기능 구현 - dto 추가 리팩토링

--- src/main/java/io/laaf/fastcampusprojectboard/controller/ArticleController.java
+++ src/main/java/io/laaf/fastcampusprojectboard/controller/ArticleController.java
@@ -32,8 +32,8 @@ public class ArticleController {
             @RequestParam(required = false) SearchType searchType,
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            ModelMap map) {
-
+            ModelMap map
+    ) {
         Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
         List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
 
@@ -47,8 +47,10 @@ public class ArticleController {
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticleWithComments(articleId));
+
         map.addAttribute("article", article);
-        map.addAttribute("articleComments", article.articleCommentResponse());
+        map.addAttribute("articleComments", article.articleCommentsResponse());
+        map.addAttribute("totalCount", articleService.getArticleCount());
 
         return "articles/detail";
     }
@@ -74,40 +76,46 @@ public class ArticleController {
     @GetMapping("/form")
     public String articleForm(ModelMap map) {
         map.addAttribute("formStatus", FormStatus.CREATE);
+
         return "articles/form";
     }
 
-    @PostMapping("/form")
+    @PostMapping ("/form")
     public String postNewArticle(ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "kaka", "asdf1234", "kaka@mail.com", "kaka", "memo", null, null, null, null
+                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
         )));
+
         return "redirect:/articles";
     }
 
     @GetMapping("/{articleId}/form")
-    public String updateArtileForm(@PathVariable Long articleId, ModelMap map) {
+    public String updateArticleForm(@PathVariable Long articleId, ModelMap map) {
         ArticleResponse article = ArticleResponse.from(articleService.getArticle(articleId));
 
         map.addAttribute("article", article);
         map.addAttribute("formStatus", FormStatus.UPDATE);
+
         return "articles/form";
     }
 
-    @PostMapping("/{articleId}/form")
-    public String updateARticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
+    @PostMapping ("/{articleId}/form")
+    public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "kaka", "asdf1234", "kaka@mail.com", "kaka", "memo", null, null, null, null
+                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
         )));
+
         return "redirect:/articles/" + articleId;
     }
 
-    @PostMapping("/{articleId}/delete")
+    @PostMapping ("/{articleId}/delete")
     public String deleteArticle(@PathVariable Long articleId) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.deleteArticle(articleId);
+
         return "redirect:/articles";
     }
+
 }
